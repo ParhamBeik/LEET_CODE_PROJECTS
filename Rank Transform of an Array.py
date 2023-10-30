d0ad5bb006b83cd5d670a97f9074df05118dfc2a
Parham <parhambeik2183@gmail.com>
2023-10-30 15:05:21 +0330

    trying to make lines fewer

--- Rank Transform of an Array.py
+++ Rank Transform of an Array.py
@@ -1,9 +1,6 @@
 arr = [37,12,28,9,100,56,80,5,12]
 listOfIndex=[]
 sortedArr=sorted(arr)
-[listOfIndex.append(sortedArr.index(number)) for number in arr]
-newList=[index+1 for index in listOfIndex]
-
-
-print(newList)
+[listOfIndex.append(sortedArr.index(number)+1) for number in arr]
+print(listOfIndex)
 
