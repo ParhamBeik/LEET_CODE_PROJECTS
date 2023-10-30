9c9647b2f45e94c676d3c35f115686b6e01f6437
Parham <parhambeik2183@gmail.com>
2023-10-30 15:38:51 +0330

    finished the code for random transform of an array

--- Rank Transform of an Array.py
+++ Rank Transform of an Array.py
@@ -2,5 +2,11 @@ arr = [37,12,28,9,100,56,80,5,12]
 listOfIndex=[]
 sortedArr=sorted(arr)
 [listOfIndex.append(sortedArr.index(number)+1) for number in arr]
+listOfRanksSimilar=[]
+[listOfRanksSimilar.append(number) for number in listOfIndex if listOfIndex.count(number)>=2 and number not in listOfRanksSimilar]
+for number in listOfIndex:
+    if number>listOfRanksSimilar[0]:
+        listOfIndex[listOfIndex.index(number)]=number-1
+print(listOfRanksSimilar)
 print(listOfIndex)
 
