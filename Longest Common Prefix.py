19698f49ee61bdbe53aa302f431512e3dc553c2a
Parham <parhambeik2183@gmail.com>
2023-11-10 10:41:41 +0330

    making changes to some files

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -1,10 +1,1 @@
 strs = ["dog","racecar","car"]
-myList=[]
-for character in strs:
-    for char in character:
-        myList.append(char)
-newList=""
-for item in myList:
-    if myList.count(item)>=len(strs) and item not in newList:
-        newList+=item
-print(newList)
